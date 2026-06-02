9680b0c17ee65a754e207d80a0e966f6321e5c24
Adya84 <94404576+Adya84@users.noreply.github.com>
2026-06-03 00:55:04 +0100

    Update __init__.py

--- custom_components/world_cup_2026/__init__.py
+++ custom_components/world_cup_2026/__init__.py
@@ -1,1 +1,14 @@
 """World Cup 2026 integration."""
+
+PLATFORMS = ["sensor"]
+
+async def async_setup_entry(hass, entry):
+    await hass.config_entries.async_forward_entry_setups(
+        entry, PLATFORMS
+    )
+    return True
+
+async def async_unload_entry(hass, entry):
+    return await hass.config_entries.async_unload_platforms(
+        entry, PLATFORMS
+    )
